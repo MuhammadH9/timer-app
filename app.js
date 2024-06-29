afa4719f14d93839d71a2059b9a9ae19584ca1d1
muhammadh9 <m.huzaif999@gmail.com>
2024-06-30 01:46:18 +0500

    up

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ startButton.addEventListener('click', () => {
     var minutes = parseInt(document.getElementById('minutes').value) || 0;
     var seconds = parseInt(document.getElementById('seconds').value) || 0;
     var totalSeconds = hours * 3600 + minutes * 60 + seconds;
-
+    
     var countdown = setInterval(() => {
         if (totalSeconds <= 0) {
             clearInterval(countdown);
             timerDisplay.textContent = 'Countdown Complete!';
+            start.disabled = false; 
         } else {
             var hoursLeft = Math.floor(totalSeconds / 3600);
             var minutesLeft = Math.floor((totalSeconds % 3600) / 60);
             var secondsLeft = totalSeconds % 60;
             timerDisplay.textContent = `${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`;
             totalSeconds--;
+            start.disabled = true; 
         }
     }, 1000);
 });
